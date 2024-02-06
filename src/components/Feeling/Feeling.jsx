b2749f526c5708191764084aa15ae5a88bc6f73e
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-05 20:45:27 -0600

    Started working on Feeling component to store value and route to next page

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 function Feeling() {
+  const [feelings, setFeelings] = useState('');
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const saveFeeling = (event) => {
+    setFeelings(event.target.value);
+    console.log(feelings);
+  };
+
+  const nextField = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'FEELING_VALUE',
+      payload: feelings,
+    });
+    history.push('/understanding');
+  };
+
   return (
     <div>
       <h3>How are you feeling today?</h3>
       <form>
-        {/* <select name="feeling" id="feel" >
-          <option value="5">5</option>
-          <option value="4">4</option>
-          <option value="3">3</option>
-          <option value="2">2</option>
-          <option value="1">1</option>
-          
-        </select> */}
-        <input type="number" step="1" min="1" max="5" />
-        <input type="submit" value="Next" />
+        <input type="number" step="1" min="1" max="5" onChange={saveFeeling} />
+        <input type="submit" value="Next" onClick={nextField} />
       </form>
     </div>
   );
