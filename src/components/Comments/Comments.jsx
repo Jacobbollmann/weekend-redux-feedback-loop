d04b098e2ab1f4eb64eb585c2c7272d366953de5
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-09 17:57:33 -0600

    Comments component successfully updating global state

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,10 +1,23 @@
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function Comments() {
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [comments, setComments] = useState('');
+
+  const updateComments = (event) => {
+    setComments(event.target.value);
+  };
 
   const nextField = (event) => {
     event.preventDefault();
+    dispatch({
+      type: 'UPDATE_COMMENTS',
+      payload: comments,
+    });
     history.push('/review');
   };
 
@@ -12,7 +25,7 @@ function Comments() {
     <div>
       <h3>Any comments you want to leave?</h3>
       <form onSubmit={nextField}>
-        <input type="text" placeholder="Comments" />
+        <input type="text" placeholder="Comments" onChange={updateComments} />
         <input type="submit" value="Next" />
       </form>
     </div>
