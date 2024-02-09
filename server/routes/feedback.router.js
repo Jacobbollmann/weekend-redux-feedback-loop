443eea22179a7db83a42fa43caf93b838401ebaf
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-09 14:32:50 -0600

    created review component

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
-
+const pool = require('../modules/pool');
 
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
-
-})
-
+  const newPost = req.body;
+  console.log(newPost);
+  res.send(newPost).status(200);
+});
 
 // DO NOT EDIT THIS ROUTE
 // This route must return all feedback.
 router.get('/', (req, res) => {
-    console.log('testing')
-    const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
-    pool.query(sqlText).then(result => {
-        res.send(result.rows)
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);
+  console.log('testing');
+  const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
