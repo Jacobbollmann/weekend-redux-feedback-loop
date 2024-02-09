43cbc2f75471738acb543577e116dfd49f4a1b1e
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-09 16:23:16 -0600

    updated store reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { storeInstance } from './store';
+import App from './components/App/App';
+import './index.css';
 
 const feedbackStore = storeInstance();
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
+const feedbackState = {
+  feeling: 0,
+  understanding: 0,
+  support: 0,
+  comments: '',
+};
+
 //Reducers will go here
+const feedback = (state = feedbackState, action) => {
+  if (action.type === 'UPDATE_FEELING') {
+    return { ...state, feeling: action.payload };
+  }
+  if (action.type === 'UPDATE_UNDERSTANDING') {
+    return { ...state, understanding: action.payload };
+  }
+  if (action.type === 'UPDATE_SUPPORT') {
+    return { ...state, support: action.payload };
+  }
+  if (action.type === 'UPDATE_COMMENTS') {
+    return { ...state, comments: action.payload };
+  }
+  return state;
+};
 
 //Create redux store
-export const storeInstance = createStore(
-  combineReducers({}),
-  applyMiddleware(logger)
-);
+export const storeInstance = () =>
+  createStore(
+    combineReducers({
+      feedback,
+    }),
+    applyMiddleware(logger)
+  );
